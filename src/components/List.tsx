a77963834bc437f17a97e38b1986bb3e002751f6
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-12-28 20:23:09 +0100

    add setItems

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+export interface IItems {
+  name: string;
+  status: string;
+}
 export const List = () => {
+  const [items, setItems] = useState<IItems[]>([]);
+
   return (
     <>
       <ul></ul>
-      <button onClick={() => {}}>+</button>
+      <button
+        onClick={() => {
+          setItems((prev) => [...prev, { name: 'pierogi', status: 'done' }]);
+        }}
+      >
+        +
+      </button>
     </>
   );
 };
