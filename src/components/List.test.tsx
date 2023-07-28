7d19257b2cd8d6fccce606801bf58ab1f5f4a670
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2023-07-28 20:58:33 +0200

    test: add test for removing

--- src/components/List.test.tsx
+++ src/components/List.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import { List } from './List';
 
-it('should have adding option enabled', () => {
+it('should have adding and deletig enabled', () => {
   render(<List />);
 
   expect(screen.getByText('+')).toBeInTheDocument();
+  expect(screen.getByText('-')).toBeInTheDocument();
 });
