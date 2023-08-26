bfa216957315c69496608f0bdac6aefa960bdc3b
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2023-08-26 12:05:29 +0200

    test: add unit test for adding todo

--- src/components/List.test.tsx
+++ src/components/List.test.tsx
@@ -1,9 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import { List } from './List';
-
-it('should have adding and deletig enabled', () => {
+import { shallow } from 'enzyme';
+it('should have adding and deleting enabled', () => {
   render(<List />);
 
   expect(screen.getByText('+')).toBeInTheDocument();
   expect(screen.getByText('-')).toBeInTheDocument();
+});
+
+it('adding new item should work', () => {
+  const wrapper = shallow(<List />);
+
+  // Check if the initial list is empty
+  expect(wrapper.find('li').length).toEqual(0);
+
+  // Simulate user input
+  const newItem = 'Apples';
+  wrapper.setState({ newItem });
+
+  // Simulate button click
+  wrapper.find('button').simulate('click');
+
+  // Check if the item was added to the list
+  expect(wrapper.find('li').length).toEqual(1);
+  expect(wrapper.find('li').text()).toEqual(newItem);
 });
